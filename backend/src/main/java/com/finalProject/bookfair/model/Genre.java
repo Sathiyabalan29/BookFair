48961b463cf00f0656101c5b4455f147bc095a58
Sathiyabalan <intsathiyabalan@gmail.com>
2026-02-13 21:25:15 +0530

    updated Genre model

--- backend/src/main/java/com/finalProject/bookfair/model/Genre.java
+++ backend/src/main/java/com/finalProject/bookfair/model/Genre.java
@@ -1,4 +1,33 @@
 package com.finalProject.bookfair.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "genres")
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "genre_name", nullable = false, unique = true)
+    private String genreName;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<User> users = new HashSet<>();
+
+    protected Genre() {
+
+    }
+
+    public Genre(String genreName) {
+        this.genreName = genreName;
+    }
+
 }
